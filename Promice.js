24310f28444a351014cb247dc4523374626cdfc1
texasjkee <gt.gluck@gmail.com>
2023-03-08 20:27:10 +0200

    refactor: myClass

--- Promice.js
+++ Promice.js
@@ -2,7 +2,26 @@ const FULFILLED = 'fulfiled';
 const PENDING = 'pending';
 const REJECTED = 'rejected';
 
-class MyPromise {}
+class MyPromise {
+	constructor(executor) {
+		this.state = PENDING;
+		this.result = undefined;
+
+		const resolve = (value) => {
+			if (this.state === PENDING) {
+				this.state = FULFILLED;
+				this.result = value;
+			}
+		}
+		
+		const reject = (error) => {
+			if (this.state === PENDING) {
+				this.state = REJECTED;
+				this.result = error;
+		}
+	}
+	executor(resolve, reject)
+}
 
 //Promise does not block the thread
 const promise = new Promise((resolve, rejected) => {
