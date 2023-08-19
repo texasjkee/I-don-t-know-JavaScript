2a5e3b330156bbaae94c6d6bd508471d9062ac91
texasjkee <gt.gluck@gmail.com>
2023-08-19 20:38:06 +0300

    fix: yield.js

--- Recursion.js
+++ Recursion.js
@@ -2,3 +2,16 @@
 //As for JS
 //> cycle
 // I don't know what that mean.
+
+// I don't know what that mean.
+// function* getNumber() {
+//   yield 1;
+//   yield 2;
+//   return 3; // note that we used a `return` here!
+// }
+
+// const iterator = getNumber();
+
+// console.log(iterator.next());
+// console.log(iterator.next());
+// console.log(iterator.next());
