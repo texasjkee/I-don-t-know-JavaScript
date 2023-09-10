b667e82f3eb21e53eb1bcfaac101d21c37caefbd
texasjkee <gt.gluck@gmail.com>
2023-09-10 22:36:23 +0300

    fix: this.js

--- This.js
+++ This.js
@@ -2,7 +2,7 @@
 //Formula of success (As for JS)
 // I don't know what that mean.
 // const o = {
-//   prop: '33-23-25',
+//   prop: '33-23-23',
 //   f: function () {
 //     return this.prop;
 //   },
