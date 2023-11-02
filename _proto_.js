3012778b847191677f9621b346c7aadafce61323
texasjkee <gt.gluck@gmail.com>
2023-11-02 21:34:45 +0200

    add: proto.js

--- _proto_.js
+++ _proto_.js
@@ -1,1 +1,14 @@
 //Proto JS
+const ShapeA = function () {};
+const ShapeB = {
+  a() {
+    console.log("aaa");
+  },
+};
+
+ShapeA.prototype.__proto__ = ShapeB;
+console.log(ShapeA.prototype.__proto__); // { a: [Function: a] }
+
+const shapeA = new ShapeA();
+shapeA.a(); // aaa
+console.log(ShapeA.prototype === shapeA.__proto__); // true
